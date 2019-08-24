494d0b1b1313991c86425db92762a25ca4c9aced
Benaja Hunzinger <benaja.hunzinger@swisscom.com>
2019-08-24 10:16:56 +0200

    added uikit

---
+++ resources/views/frontpage.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Test</h1>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bio-Imkerei Hunzinger</title>
+    <link rel="stylesheet" type="text/css" href="/css/uikit-rtl.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/uikit.min.css">
 </head>
 <body>
-    
+    @yield('content')
+    <script src="/js/uikit-icons.min.js"></script>
+    <script src="/js/uikit.min.js"></script>
 </body>
 </html>
